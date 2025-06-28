c955a62f342cef8d825d9d373330d25a58f1cd8e
Ian McEwen <iwm2002@gmail.com>
2025-06-27 23:18:31 -0400

    Format Image and add projects section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home-component/home-component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent, title: 'Home' }
+    { path: '', component: HomeComponent }
 ];
